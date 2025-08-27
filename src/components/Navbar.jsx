97d1084c18f31119251cd8c5e6d28f22a9b5167a
ashleyCodie <a.brooks9385@gmail.com>
2025-08-27 08:20:33 -0700

    Updated Logo size and background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,70 +5,70 @@ import { Link, useNavigate, useLocation } from "react-router";
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <nav className="bg-purple-300 border-gray-200 dark:bg-purple-300">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img src={logo} className="h-45" alt="Logo" />
-        </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium text-3xl flex flex-col p-4 md:p-0 mt-4 border border-purple-300 rounded-lg bg-purple-300 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-purple-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/Projects"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/ContactMe"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-=======
+
+    // <nav className="bg-purple-300 border-gray-200 dark:bg-purple-300">
+    //   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    //     <Link
+    //       to="/"
+    //       className="flex items-center space-x-3 rtl:space-x-reverse"
+    //     >
+    //       <img src={logo} className="h-45" alt="Logo" />
+    //     </Link>
+    //     <button
+    //       data-collapse-toggle="navbar-default"
+    //       type="button"
+    //       className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    //       aria-controls="navbar-default"
+    //       aria-expanded="false"
+    //     >
+    //       <span className="sr-only">Open main menu</span>
+    //       <svg
+    //         className="w-5 h-5"
+    //         aria-hidden="true"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         fill="none"
+    //         viewBox="0 0 17 14"
+    //       >
+    //         <path
+    //           stroke="currentColor"
+    //           strokeLinecap="round"
+    //           strokeLinejoin="round"
+    //           strokeWidth="2"
+    //           d="M1 1h15M1 7h15M1 13h15"
+    //         />
+    //       </svg>
+    //     </button>
+    //     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+    //       <ul className="font-medium text-3xl flex flex-col p-4 md:p-0 mt-4 border border-purple-300 rounded-lg bg-purple-300 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-purple-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    //         <li>
+    //           <Link
+    //             to="/"
+    //             className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+    //           >
+    //             Home
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link
+    //             to="/Projects"
+    //             className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+    //           >
+    //             Projects
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link
+    //             to="/ContactMe"
+    //             className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-purple-300 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+    //           >
+    //             Contact
+    //           </Link>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+
     
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -102,7 +102,7 @@ const Navbar = () => {
 
   
 
->>>>>>> 6172fe4391f45bf1f1ae4c36c2b5b15e5cf9adb6
+
   );
 };
 
